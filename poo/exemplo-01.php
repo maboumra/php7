50babb4052015010f84de6bac56e64aa70283d6a
Marcelo Aboumrad <comercial@ijk.com.br>
2017-07-21 14:20:01 -0300

    POO example with cpf validation routine

---
+++ poo/exemplo-01.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: y1rj
+ * Date: 21/07/2017
+ * Time: 13:56
+ */
+class Pessoa {
+
+    private static $count = 0;
+
+    public function __construct($nome="José")
+    {
+        $this->nome = $nome;
+        Pessoa::$count++;
+    }
+
+    private $nome;
+
+    public function falar(){
+        return "Meu nome é $this->nome";
+    }
+
+    public static function getCount(){
+        return Pessoa::$count;
+    }
+
+    public static function valida_cpf($cpf) {
+        if(empty($cpf)) {
+            return false;
+        }
+
+        $cpf = preg_match('/[0-9]/', $cpf)?$cpf:0;
+
+        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+
+        if (strlen($cpf) != 11) {
+            echo "length";
+            return false;
+        }
+
+        else if ($cpf == '00000000000' ||
+            $cpf == '11111111111' ||
+            $cpf == '22222222222' ||
+            $cpf == '33333333333' ||
+            $cpf == '44444444444' ||
+            $cpf == '55555555555' ||
+            $cpf == '66666666666' ||
+            $cpf == '77777777777' ||
+            $cpf == '88888888888' ||
+            $cpf == '99999999999') {
+            return false;
+
+        } else {
+
+            for ($t = 9; $t < 11; $t++) {
+
+                for ($d = 0, $c = 0; $c < $t; $c++) {
+                    $d += $cpf{$c} * (($t + 1) - $c);
+                }
+                $d = ((10 * $d) % 11) % 10;
+                if ($cpf{$c} != $d) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+}
+
+$p1 = new Pessoa("João");
+echo $p1->falar();
+
+echo "<br>";
+
+$p2 = new Pessoa("Gláucio");
+echo $p2->falar();
+
+echo "<br>";
+
+$p3 = new Pessoa;
+echo $p3->falar();
+
+echo "<br>";
+echo Pessoa::getCount()." instâncias";
